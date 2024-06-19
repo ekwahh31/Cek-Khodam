81dd22c84ce0fda593fe2b31a8662f6b08944ff5
ekwahh31 <ekabintangcoys@gmail.com>
2024-06-19 07:10:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document
+  .getElementById("nameform")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const nama = document.getElementById("inputnama").value;
+    const khodam = [
+      "MACAN PUTIH",
+      "KUAH SOTO",
+      "KUTU LUMPUH",
+      "MOBILE REMOTE",
+      "CICAK SUMBING",
+      "KUTU AIR",
+      "KUDA STANDING",
+      "TUYUL BIRU",
+      "CUMI MULLET",
+    ];
+    const randomKhodam = khodam[Math.floor(Math.random() * khodam.length)];
+
+    const hasilDiv = document.getElementById("hasil");
+    hasilDiv.textContent = `${nama} , khodam kamu adalah ${randomKhodam}`;
+  });
